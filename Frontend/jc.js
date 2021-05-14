d9cc63efc6b2830768cdbf9ba8b6cb2cc232b73e
Martin <martindaa@hotmail.no>
2021-05-14 15:56:23 +0200

    Info button + summary and desciption

--- Frontend/jc.js
+++ Frontend/jc.js
@@ -1,46 +1,49 @@
+var cart = [];
+var products = [];
 
-var users =[]
-var array=[]
+function add_cart(){
 
-var h_row1=null
+}
 
-var x = document.getElementsByClassName("pris1")
-
-console.log(x)
-
-
-var y = document.getElementsByClassName("name1")
-
-
-var z= {navn : y, pris: x }
-
-array[0]=z
-
-
+function more_info(){
+    row = this.parentElement.parentElement;
+    var describ = products[row.id][4];
+    console.log(`List: ${describ}`)
+    var h_describ = row.querySelector(".card-text")
+    h_describ.innerHTML = describ;
+    
+}
 
 function the_func(data){
     var h_list= document.getElementById("container");
-    if(h_row1==null){
-        h_row1=document.getElementById("0");
-    }
+    var h_row1=document.getElementById("0");
+  
     console.log('My DOM list: '+h_list);
     console.log('data length:'+ data.length +'data: '+data);
 
     for(var i=0; i < data.length; i++){
         console.log('item rendering: '+data[i]);
 
-
-        [idP, nameP, price] =data[i];
+        products[i] = data[i];
+        [idP, nameP, price, summary, description] = data[i];
 
         var cloned_row = h_row1.cloneNode(true);
         cloned_row.id =i;
 
         var h_title = cloned_row.querySelector(".name");
-        h_title.innerHTML=idP.toString() +". Name: "+ nameP;
+        h_title.innerHTML=idP.toString() +" Name: "+ nameP;
 
         var h_pris = cloned_row.querySelector(".pris");
         h_pris.innerHTML= price;
 
+        var h_summary = cloned_row.querySelector(".card-text");
+        h_summary.innerHTML = summary;
+
+        var h_btn1 = cloned_row.querySelector(".btn1");
+        h_btn1.onclick = more_info;
+
+        var h_btn2 = cloned_row.querySelector(".btn2");
+        h_btn2.onclick = add_cart;
 
         h_list.insertBefore(cloned_row, h_list.querySelector(".container"));
         h_list.appendChild(cloned_row);
@@ -48,7 +51,6 @@ function the_func(data){
         document.body.appendChild(h_list);
 
         console.log("test");
-
     }
 }
 
--- sql/db.sql
+++ sql/db.sql
@@ -6,14 +6,17 @@ CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT,
     name varchar(32),
     price int,
+    summary varchar(255),
+    description mediumtext,
     PRIMARY KEY (id)
 );
 
-INSERT INTO products (name, price) VALUES
-('linsesuppe', 100),
-('ertesuppe', 125),
-('brokkolisuppe', 150),
-('brenneslesuppe', 75);
+INSERT INTO products (name, price, summary, description) VALUES
+('Lentil soup', 100, 'A tasty, simple and easy to make soup, with a fresh taste of ginger and orange', 'Ingredients: One onion, two cloves garlic, three carrots, one and a half vegetable broth, one tablespoon ginger, one orange, 200g lentil, half a teaspoon turmeric and fresh coriander.'),
+('Yellow pea soup', 125, 'Traditional pea soup with pork knuckle suitable all year round', 'Ingredients: One lightly salted pork knuckle, 200g dried yellow peas, one onion, one celery root, two bay leaf, two liters water, two carrots, two leeks and four thyme stalks'),
+('Broccoli soup', 150, 'A healthy broccoli soup that is made quickly.', 'Ingredients: One broccoli, two cloves garlic, one liter vegetable broth, three tablespoons olive oil, basil, chives, mint, salt and pepper.'),
+('Stinging nettle soup', 75, 'Stinging nettle is annoying to deal with, but it makes a great soup, and is full of useful vitamins!', 'Ingredients: One shallot, two gloves garlic, two tablespoons butter, one liter of nettle, six deciliter chicken broth, one deciliter cream, one handful of fresh thyme, salt and pepper');
+
 
 CREATE USER IF NOT EXISTS'anonymous' IDENTIFIED BY 'pikachoo';
 GRANT SELECT ON products TO 'anonymous';
